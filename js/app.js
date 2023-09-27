6fea25ef7bef4d798a041018854a907fa1dfdb93
mmarentette <melrosemarentette@gmail.com>
2023-09-27 11:52:27 -0400

    Completed exercises 1 and 2

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 // log something to make sure the file is being loaded
 console.log('My app.js is being run!');
+
+/*
+Exercise 1:
+  - Define an empty array named foods
+*/
+
+// Exercise 1 has been completed for you...
+
+const foods = [];
+
+console.log('Exercise 1 Result:\n', foods);
+
+/*
+Exercise 2:
+  - Add the strings 'pizza' & 'cheeseburger' to the foods array such that 'pizza' comes before 'cheeseburger'.
+*/
+
+// Complete Exercise 2 below...
+
+// Push method adds item(s) to the end of an array; in this case, 'pizza' and 'cheeseburger'
+foods.push('pizza', 'cheeseburger');
+
+// Output updated foods array to the console
+console.log('Exercise 2 Result:\n',  foods);
